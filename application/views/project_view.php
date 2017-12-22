75d85c8efdc7700c8b5ee534e55e3e86a1e55089
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-12-22 15:07:18 +0530

    f

--- application/views/project_view.php
+++ application/views/project_view.php
@@ -297,7 +297,7 @@
                               ?>
                           <div class="col-md-10">
                             <div class="panel panel-default">
-                              <div class="panel-heading">User Name:<?php echo $row1->userName ?> User</div>
+                              <div class="panel-heading">User Name :<?php echo $row1->userName ?></div>
                               <div class="panel-body"><?php echo $row->text ?></div>
                             </div>
                           </div>
