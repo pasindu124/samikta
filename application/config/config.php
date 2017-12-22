f3f410f22d5e7f012366a8a825b632d602ff65e3
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-12-22 13:01:15 +0530

    cp

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |as
 */
-//$config['base_url'] = 'http://34.227.47.37/samikta/'
-$config['base_url'] = 'http://localhost/samikta/';
+$config['base_url'] = 'http://34.227.47.37/samikta/'
+//$config['base_url'] = 'http://localhost/samikta/';
 
 /*
 |--------------------------------------------------------------------------
